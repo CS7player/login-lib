75eb6696c451e4a4f55c964f0e8190d29cff307e
chandra <s.chandrasekhar.h@gmail.com>
2025-05-21 20:45:38 +0530

    check01

--- library/index.js
+++ library/index.js
@@ -2,6 +2,7 @@ require("./config-env");
 require("./msg-code");
 exports.jwt = require("./jwt");
 exports.pbkdf = require("./pbkdf");
+exports.otp = require("./oauth2-otp");
 exports.mongoQuery = require("./../mongoose-library").mongoHelper;
 exports.mongoConnection = require("./../mongoose-library").connectDB;
 exports.mongoObjId = require("./../mongoose-library").getObjectId;
