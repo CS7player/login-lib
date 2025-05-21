043f1188562b75c20d7dbcb290a3bd1ba1079d60
chandra <s.chandrasekhar.h@gmail.com>
2025-05-21 20:38:19 +0530

    check

--- library/config-env.js
+++ library/config-env.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 //port_number
 global.PORT = process.env.PORT || 3000;
-global.ALLOW_ORIGNS = process.ALLOW_ORIGNS.split(',') || [];
+global.ALLOW_ORIGNS = (process.env.ALLOW_ORIGNS || '').split(',').filter(Boolean);
 global.JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || "jwt";
 
 //Database_connection_info
